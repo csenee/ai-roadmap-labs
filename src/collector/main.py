f372a5418afbe3215c115c14747043c9d7afa8f4
csenee <enricop891@gmail.com>
2026-02-10 15:24:02 +0100

    Comments and last improvements

--- src/collector/main.py
+++ src/collector/main.py
@@ -12,23 +12,27 @@ os.makedirs(LOG_DIR, exist_ok=True)
 
 base_url = "https://pokeapi.co/api/v2/"
 
+#logging in File:
 file_handler = logging.FileHandler(os.path.join(LOG_DIR, "app.log"))
 file_handler.setFormatter(
     logging.Formatter("%(asctime)s  %(levelname)s  %(message)s")
 )
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s  %(levelname)s  %(message)s",
-)
+
+#für logging in Terminal:
+#logging.basicConfig(
+#    level=logging.INFO,
+#    format="%(asctime)s  %(levelname)s  %(message)s",
+#)
 logger = logging.getLogger(__name__)
 
 
 logger.addHandler(file_handler)
 
-
+#erstellt data falls nicht vorher vorhanden
 def ensure_data_dir():
     os.makedirs(DATA_DIR, exist_ok=True)
 
+#führt api call durch
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     logger.info("Requesting %s", url)
@@ -41,15 +45,17 @@ def get_pokemon_info(name):
     else: 
         logger.error("Request failed %s", response.status_code)
         return None
-    
+
+#sichert pokemon info als individuelles json file    
 def save_json(data):
     ensure_data_dir()
-    filename = f"{data["name"]}.json" 
+    filename = f"{data['name']}.json" 
     path = os.path.join(DATA_DIR, filename)
     with open(path, "w") as f:
         json.dump(data, f, indent=2)
         logger.info("Saved -> %s", path)
 
+#führt allgmeinen code aus
 def main():
     pokemon_name = sys.argv[1] if len(sys.argv) > 1 else "charizard"
     pokemon_name = pokemon_name.lower()
@@ -57,11 +63,12 @@ def main():
     pokemon_info = get_pokemon_info(pokemon_name)
 
     if pokemon_info:
-        print(f"Name: {pokemon_info["name"].capitalize()}")
-        print(f"Height: {pokemon_info["height"]}")
-        print(f"Weight: {pokemon_info["weight"]}")
+        print(f"Name: {pokemon_info['name'].capitalize()}")
+        print(f"Height: {pokemon_info['height']}")
+        print(f"Weight: {pokemon_info['weight']}")
         save_json(pokemon_info)
         logger.info("Done")
 
+#als modul denkbar
 if __name__ == "__main__":
     main()
